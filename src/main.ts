2caeefd2303929cc861a55ed0327be290fc9107f
Hemraj Budha <budhah282@gmail.com>
2025-08-12 11:30:15 +0545

    Your message

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,11 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+    // Use Render's assigned port or fallback to 3000
+  const port = process.env.PORT || 3000;
+
+  await app.listen(port);
+
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
